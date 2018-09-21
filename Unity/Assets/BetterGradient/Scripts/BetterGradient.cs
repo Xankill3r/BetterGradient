3b5d039c6eb2a28e7a2ae7814a49e9d3dcb8b23e
Ujwal Kumar <ujwal.gamedev@gmail.com>
2018-09-22 00:41:14 +0530

    Fixed bug in Gradient to Texture Method causing wrong calculation of color values at last index Added Method for approximating an arbitrary array of colors as an 8-key Gradient

--- Unity/Assets/BetterGradient/Scripts/BetterGradient.cs
+++ Unity/Assets/BetterGradient/Scripts/BetterGradient.cs
@@ -5,14 +5,33 @@ using UnityEngine.Assertions;
 
 namespace BetterGradient
 {
-    public static class BetterGradient
+    public enum GradientMode
     {
-        public enum GradientMode
+        RGB,
+        LChuv
+    }
+
+    public struct ErrorData
+    {
+        public readonly double AverageDifference;
+        public readonly double StdDeviation;
+        public readonly double MaxDeviation;
+
+        public ErrorData(double averageDifference, double stdDeviation, double maxDeviation)
         {
-            RGB,
-            LChuv
+            AverageDifference = averageDifference;
+            StdDeviation = stdDeviation;
+            MaxDeviation = maxDeviation;
         }
 
+        public override string ToString()
+        {
+            return $"ErrorData (Avg:{AverageDifference}, StdDev:{StdDeviation}, MaxDev:{MaxDeviation})";
+        }
+    }
+
+    public static class BetterGradient
+    {
         /// <summary>
         /// Generates a texture that encodes the Gradient specified by colors, colorKeyLocations, alpha and alphaKeyLocations.
         /// The generated texture is textureWidth x 1 pixels in size.
@@ -24,7 +43,7 @@ namespace BetterGradient
         /// <param name="alphaKeyLocations"></param>
         /// <param name="textureWidth"></param>
         /// <returns></returns>
-        public static bool GradientToColors(Color[] colors, float[] colorKeyLocations, float[] alphas, float[] alphaKeyLocations, ref Color[] gradientColors, GradientMode mode)
+        public static bool GradientToColors(Color[] colors, float[] colorKeyLocations, float[] alphas, float[] alphaKeyLocations, Color[] gradientColors, GradientMode mode)
         {
             Assert.AreEqual(colors.Length, colorKeyLocations.Length);
             Assert.AreEqual(alphas.Length, alphaKeyLocations.Length);
@@ -38,19 +57,13 @@ namespace BetterGradient
             float prevAlphaKey = 0f, nextAlphaKey = alphaKeyLocations[0];
             float prevAlpha = alphas[0], nextAlpha = alphas[0];
             var textureWidth = gradientColors.Length;
+            var invTextureWidth = 1f / (textureWidth - 1);
             for (int i = 0; i < textureWidth; i++)
             {
-                float f = i / (float)textureWidth;
+                float f = i * invTextureWidth;
                 if (f >= nextAlphaKey)
                 {
-                    if (nextAlphaKey == alphaKeyLocations[alphaKeyLocations.Length - 1])
-                    {
-                        // Last key is before 1f
-                        prevAlphaKey = alphaKeyLocations[alphaKeyLocations.Length - 1];
-                        nextAlphaKey = 1f;
-                        prevAlpha = nextAlpha = alphas[alphas.Length - 1];
-                    }
-                    else
+                    if (nextAlphaKey < 1f)
                     {
                         prevAlphaKey = nextAlphaKey;
                         prevAlpha = nextAlpha;
@@ -65,14 +78,7 @@ namespace BetterGradient
                 }
                 if (f >= nextColorKey)
                 {
-                    if (nextColorKey == colorKeyLocations[colorKeyLocations.Length - 1])
-                    {
-                        // Last key is before 1f
-                        prevColorKey = colorKeyLocations[colorKeyLocations.Length - 1];
-                        nextColorKey = 1f;
-                        prevColor = nextColor = colors[colors.Length - 1];
-                    }
-                    else
+                    if (nextColorKey < 1f)
                     {
                         prevColorKey = nextColorKey;
                         prevColor = nextColor;
@@ -101,6 +107,158 @@ namespace BetterGradient
                 gradientColors[i] = new Color(lerpedColor.r, lerpedColor.g, lerpedColor.b, lerpedAlpha);
             }
             return true;
+        }
+
+        /// <summary>
+        /// Converts an array of colors into an approximate Gradient using a max of 8 color keys and 8 alpha keys
+        /// </summary>
+        /// <param name="colors"></param>
+        /// <param name="gradient"></param>
+        /// <param name="approxColors"></param>
+        /// <param name="approxAlpha"></param>
+        /// <returns>Error data specifying deviation from ideal case</returns>
+        public static ErrorData ApproximateColorArrayAsGradient(Color[] colors, Gradient gradient, List<GradientColorKey> approxColors, List<GradientAlphaKey> approxAlpha)
+        {
+            approxAlpha.Clear();
+            approxColors.Clear();
+
+            float invLength = 1f / (colors.Length - 1);
+            // Init the lists
+            for (int i = 0; i < colors.Length - 1; i++)
+            {
+                // Add first alpha value that deviates from front
+                if (colors[i].a != colors[i + 1].a)
+                {
+                    var t = i * invLength;
+                    approxAlpha.Add(new GradientAlphaKey(colors[i].a, t));
+                    break;
+                }
+            }
+            for (int i = 0; i < colors.Length - 1; i++)
+            {
+                // Add first color value that deviates from front
+                if (colors[i].r != colors[i + 1].r || colors[i].g != colors[i + 1].g || colors[i].b != colors[i + 1].b)
+                {
+                    var t = i * invLength;
+                    approxColors.Add(new GradientColorKey(colors[i], t));
+                    break;
+                }
+            }
+            for (int i = colors.Length - 1; i > 0; i--)
+            {
+                // Add first alpha value that deviates from back
+                if (colors[i].a != colors[i - 1].a)
+                {
+                    var t = i * invLength;
+                    approxAlpha.Add(new GradientAlphaKey(colors[i].a, t));
+                    break;
+                }
+            }
+            for (int i = colors.Length - 1; i > 0; i--)
+            {
+                // Add first color value that deviates from back
+                if (colors[i].r != colors[i - 1].r || colors[i].g != colors[i - 1].g || colors[i].b != colors[i - 1].b)
+                {
+                    var t = i * invLength;
+                    approxColors.Add(new GradientColorKey(colors[i], t));
+                    break;
+                }
+            }
+            gradient.SetKeys(approxColors.ToArray(), approxAlpha.ToArray());
+
+            int iters = 8;
+            while (iters-- > 0)
+            {
+                float maxColorDistance = 0f, maxAlphaDistance = 0f;
+                int maxColorDistanceIndex = -1, maxAlphaDistanceIndex = -1;
+                for (int i = 0; i < colors.Length; i++)
+                {
+                    var gradientColor = gradient.Evaluate(i * invLength);
+                    var alphaDistance = Mathf.Abs(colors[i].a - gradientColor.a);
+                    var colorDistance = ColorRGBSqrDistance(colors[i], gradientColor);
+                    if (alphaDistance > maxAlphaDistance && alphaDistance > 1e-3f)
+                    {
+                        maxAlphaDistance = alphaDistance;
+                        maxAlphaDistanceIndex = i;
+                    }
+                    if (colorDistance > maxColorDistance && colorDistance > 1e-3f)
+                    {
+                        maxColorDistance = colorDistance;
+                        maxColorDistanceIndex = i;
+                    }
+                }
+                // Break if no deviations found
+                if (maxColorDistanceIndex == -1 && maxAlphaDistanceIndex == -1)
+                {
+                    break;
+                }
+                // Insert new keys
+                if (maxAlphaDistanceIndex != -1)
+                {
+                    var newKeyLocation = maxAlphaDistanceIndex * invLength;
+                    var newAlpha = colors[maxAlphaDistanceIndex].a;
+                    for (int i = 0; i < approxAlpha.Count; i++)
+                    {
+                        if (newKeyLocation > approxAlpha[i].time)
+                        {
+                            approxAlpha.Insert(i + 1, new GradientAlphaKey(newAlpha, newKeyLocation));
+                            break;
+                        }
+                    }
+                }
+                if (maxColorDistanceIndex != -1)
+                {
+                    var newKeyLocation = maxColorDistanceIndex * invLength;
+                    var newColor = colors[maxColorDistanceIndex];
+                    for (int i = 0; i < approxColors.Count; i++)
+                    {
+                        if (newKeyLocation > approxColors[i].time)
+                        {
+                            approxColors.Insert(i + 1, new GradientColorKey(newColor, newKeyLocation));
+                            break;
+                        }
+                    }
+                }
+                // Update gradient
+                gradient.SetKeys(approxColors.ToArray(), approxAlpha.ToArray());
+                // Break if we have used maximum allowed Color and Alpha keys
+                if (approxAlpha.Count == 8 || approxColors.Count == 8)
+                {
+                    break;
+                }
+            }
+
+            // Calculate error values
+            var totalErr = 0f;
+            var maxErr = 0f;
+            for (int i = 0; i < colors.Length; i++)
+            {
+                var approx = gradient.Evaluate(i / (float)(colors.Length - 1));
+                var error = Mathf.Sqrt(ColorRGBASqrDistance(approx, colors[i]));
+                totalErr += error;
+                maxErr = Mathf.Max(maxErr, error);
+            }
+            var avg = totalErr / colors.Length;
+            var totalDeviation = 0f;
+            for (int i = 0; i < colors.Length; i++)
+            {
+                var approx = gradient.Evaluate(i / (float)(colors.Length - 1));
+                var error = Mathf.Sqrt(ColorRGBASqrDistance(approx, colors[i]));
+                var deviation = (error - avg) * (error - avg);
+                totalDeviation += deviation;
+            }
+            var stdDev = Mathf.Sqrt(totalDeviation / colors.Length);
+
+            return new ErrorData(avg, stdDev, maxErr);
+        }
+
+        private static float ColorRGBSqrDistance(Color a, Color b)
+        {
+            return (a.r - b.r) * (a.r - b.r) + (a.g - b.g) * (a.g - b.g) + (a.b - b.b) * (a.b - b.b);
+        }
+        private static float ColorRGBASqrDistance(Color a, Color b)
+        {
+            return (a.r - b.r) * (a.r - b.r) + (a.g - b.g) * (a.g - b.g) + (a.b - b.b) * (a.b - b.b) + (a.a - b.a) * (a.a - b.a);
         }
     }
 }
